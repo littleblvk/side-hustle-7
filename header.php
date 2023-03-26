c54e8b037d89941caae989dab15a5530d838c0f0
littleblvk <113787853+littleblvk@users.noreply.github.com>
2023-03-26 20:32:49 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+function multiply( $num1,$num2, $num3 =5){
+    $answer =  $num1 * $num2 * $num3 ;
+    return $answer;
+}
+ echo multiply(2,1);
+ ?>
+ <?php
+ include 'class.php';
+ ?>
+
+
+
+
+
